377b3b6ee284333def72eab2b17efe1d812777a7
Dennis Vestergaard Værum <github@varum.dk>
2020-11-29 22:49:15 +0100

    updated git-ask-pass to handle cases there the username for a domain is not specified

--- .local/bin/git-ask-pass.sh
+++ .local/bin/git-ask-pass.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 # ex: set tabstop=8 softtabstop=0 expandtab shiftwidth=2 smarttab:
 
-set -x
+set -eu
+[ -n "${BASH_DEBUG:-}" ] && set -x
 
 ### 2 examples for possible values for argument INPUT
 # Password for 'https://github.com':
 # Password for 'https://dvaerum@github.com':
 INPUT="${1}"
 
+if grep --quiet --ignore-case '^username' <<<${INPUT}; then
+  HOST="$(sed -E -e "s|^username for '(https?://)?([^@]+@)?([^']+)': *\$|\1\3|i" <<<${INPUT})"
+  >&2 echo "You have not specified a username for this domain, plz do so using the following command:"
+  >&2 echo "  git config --global credential.${HOST}.username YOUR-USERNAME"
+  exit 1
+fi
 
-HOST="$(sed -E -e "s|^password for '(https?://)?([^@]+@)?([^']+)': *\$|\3|i" <<<$INPUT)"
-USER="$(sed -E -e "s|^password for '(https?://)?([^@]+@)?([^']+)': *\$|\2|i" -e 's|@||' <<<$INPUT)"
-
+HOST="$(sed -E -e "s|^password for '(https?://)?([^@]+@)?([^']+)': *\$|\3|i" <<<${INPUT})"
+USER="$(sed -E -e "s|^password for '(https?://)?([^@]+@)?([^']+)': *\$|\2|i" -e 's|@||' <<<${INPUT})"
 
 if [ "${HOST}" == "github.com" ]; then
   pass "website/${HOST}" | head -1
 elif [ "${HOST}" == "gitlab.com" ]; then
   pass "website/${HOST}" | head -1
 else
-  pass "website/${HOST}" | head -1
+  exit 1
 fi
 
